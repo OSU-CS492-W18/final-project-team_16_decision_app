b45fe1356a39ec2de977da463b87cf17b40ae6b3
srb76 <bonnersa@oregonstate.edu>
2018-03-22 16:22:04 -0700

    Added addFactor to ChoosyDatabase and constructors for decsion and factor objects
    -also added test to mainactivity

--- app/src/main/java/edu/oregonstate/choosy/DecisionUtils.java
+++ app/src/main/java/edu/oregonstate/choosy/DecisionUtils.java
@@ -12,9 +12,28 @@ public class DecisionUtils {
         public String firstOption;
         public String secondOption;
 
+        public decisionObject(String first, String second) {
+            firstOption = first;
+            secondOption = second;
+        }
+
         public String getString() {
             String line = firstOption + " vs " + secondOption;
             return line;
+        }
+    }
+
+    public static class factorObject implements Serializable {
+        public String name;
+        public String comp;
+        public int pro;
+        public int weight;
+
+        public factorObject(String factorName, String link, int isPro, int sliderWeight) {
+            name = factorName;
+            comp = link;
+            pro = isPro;
+            weight = sliderWeight;
         }
     }
 

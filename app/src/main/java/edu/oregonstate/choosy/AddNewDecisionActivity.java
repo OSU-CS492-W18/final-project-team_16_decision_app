3172637529b4e7ae9c3e98dd30e32cbe942be0f9
lostliddell <cheshirestray@gmail.com>
2018-03-22 23:32:11 -0700

    quotes now displaying with asynctask

--- app/src/main/java/edu/oregonstate/choosy/AddNewDecisionActivity.java
+++ app/src/main/java/edu/oregonstate/choosy/AddNewDecisionActivity.java
@@ -2,12 +2,59 @@ package edu.oregonstate.choosy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class AddNewDecisionActivity extends AppCompatActivity {
+    private TextView quoteView;
+    private final static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_decision);
+        quoteView = (TextView)findViewById(R.id.TV_quote);
+        getQuote();
+    }
+
+    private void getQuote() {
+        String quoteURL = QuoteUtils.buildQuoteURL("inspire");
+        new QuoteFetchTask().execute(quoteURL);
+    }
+
+    public class QuoteFetchTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String quoteURL = urls[0];
+            String quote = null;
+            try {
+                quote = NetworkUtils.doHTTPGet(quoteURL);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return quote;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null) {
+                String quote = QuoteUtils.parseQuoteJSON(s);
+                Log.d(TAG, "quote fetched: " + quote);
+                quoteView.setText(quote);
+            }
+        }
     }
 }
--- app/src/main/java/edu/oregonstate/choosy/QuoteUtils.java
+++ app/src/main/java/edu/oregonstate/choosy/QuoteUtils.java
@@ -29,9 +29,13 @@ public class QuoteUtils {
             JSONObject quoteObj = new JSONObject(quoteJSON);
             JSONObject quoteObj2 = quoteObj.getJSONObject("contents");
             JSONArray quoteArray1 = quoteObj2.getJSONArray("quotes");
-            String quote = quoteArray1.getString(0);
+            String quoteinfo = quoteArray1.getString(0);
+            JSONObject quoteObj3 = new JSONObject(quoteinfo);
+            String quote = quoteObj3.getString("quote");
+            String author = quoteObj3.getString("author");
+            String finalquote = quote + " - " + author;
             Log.d(TAG, "quote string: " + quote);
-            return quote;
+            return finalquote;
         } catch (JSONException e) {
             e.printStackTrace();
             return null;

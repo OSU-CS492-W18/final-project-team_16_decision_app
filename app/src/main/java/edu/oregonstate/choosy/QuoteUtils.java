6d2b7ca636f3e2271c27baa8461948c0011e5cfb
lostliddell <cheshirestray@gmail.com>
2018-03-22 21:48:41 -0700

    quoteutils fixed, should be finished

--- app/src/main/java/edu/oregonstate/choosy/QuoteUtils.java
+++ app/src/main/java/edu/oregonstate/choosy/QuoteUtils.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.util.Log;
 
 import java.io.Serializable;
 import java.text.ParseException;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 public class QuoteUtils {
     private final static String QUOTE_BASE_URL = "https://quotes.rest/qod.json";
     private final static String QUOTE_QUERY_PARAM = "&category=";
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     public static String buildQuoteURL(String quotetype) {
         return Uri.parse(QUOTE_BASE_URL).buildUpon()
@@ -27,7 +29,8 @@ public class QuoteUtils {
             JSONObject quoteObj = new JSONObject(quoteJSON);
             JSONObject quoteObj2 = quoteObj.getJSONObject("contents");
             JSONArray quoteArray1 = quoteObj2.getJSONArray("quotes");
-            String quote = quoteArray1.getString("quote");
+            String quote = quoteArray1.getString(0);
+            Log.d(TAG, "quote string: " + quote);
             return quote;
         } catch (JSONException e) {
             e.printStackTrace();
